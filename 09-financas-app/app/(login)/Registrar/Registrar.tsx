a45870380f2e0f03d4c57577a241364a48d4cc0f
leonardo-gonsalez-clear <leonardo.gonsalez.clear@gmail.com>
2023-08-21 10:55:59 -0300

    ✅ 09. 92: signUp page

--- 09-financas-app/app/(login)/Registrar/Registrar.tsx
+++ 09-financas-app/app/(login)/Registrar/Registrar.tsx
@@ -1,11 +1,17 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Container } from './styled'
+import Input from '../../../components/Input'
+import Button from '../../../components/Button'
 
 const Registrar = () => {
   return (
-    <View>
-      <Text>Registrar</Text>
-    </View>
+    <Container>
+      <Input label="Nome" placeholder='Digite seu nome' />
+      <Input label="Email" placeholder='exemplo@email.com' />
+      <Input label="Senha" placeholder='************' />
+      <Button>Criar conta</Button>
+    </Container>
   )
 }
 
--- 09-financas-app/app/(login)/_layout.tsx
+++ 09-financas-app/app/(login)/_layout.tsx
@@ -6,8 +6,19 @@ const Layout = () => {
   return (
     <Stack initialRouteName='Login/index' screenOptions={{ contentStyle: { padding: 16 } }}>
       <Stack.Screen name='index' redirect={true} />
-      <Stack.Screen name='Login/index' options={{ headerShown: false }} />
-      <Stack.Screen name='Registrar/index' options={{ title: "Registrar" }} />
+      <Stack.Screen name='Login/index' options={{
+        headerShown: false,
+        statusBarStyle: "dark",
+        statusBarTranslucent: true
+      }} />
+      <Stack.Screen name='Registrar/index' options={{
+        title: "Registrar",
+        headerStyle: { backgroundColor: "#1e81b0" },
+        headerTintColor: "#fff",
+        statusBarStyle: "light",
+        statusBarTranslucent: true
+
+      }} />
     </Stack>
   )
 }
