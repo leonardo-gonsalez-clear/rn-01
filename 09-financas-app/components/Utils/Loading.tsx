d2e930480b5fdfc678f8d9ee3cc1ea5579808664
leonardo-gonsalez-clear <leonardo.gonsalez.clear@gmail.com>
2023-08-22 10:22:16 -0300

    ✅ 09. 95: adding loading to buttons

---
+++ 09-financas-app/components/Utils/Loading.tsx
@@ -0,0 +1,10 @@
+import { View, Text, ActivityIndicator } from 'react-native'
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <ActivityIndicator color={"#fff"} size={20} />
+  )
+}
+
+export default Loading
