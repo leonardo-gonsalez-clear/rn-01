30dad993e8cc5ca10eebc2d6a91a75a98f77e2b9
leonardo-gonsalez-clear <leonardo.gonsalez.clear@gmail.com>
2023-08-29 08:44:31 -0300

    ✅ 15. 141-142: initial setting map

---
+++ 15-maps/app/(tabs)/index.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native';
+import { View } from '../../components/Themed';
+import MapView from 'react-native-maps';
+
+export default function TabOneScreen() {
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%'
+  }
+});
